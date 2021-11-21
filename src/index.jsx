3886462d7f977aabfe4ab7f8d4cfb7701cb2ec2c
nightorb <ana.nath@gmx.de>
2021-11-22 00:03:06 +0100

    update index.jsx
    import main-view component

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MainView from './components/main-view/main-view';
 
 // import statement to indicate that index.scss needs to be bundled
 import './index.scss';
@@ -8,9 +9,7 @@ import './index.scss';
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix">
-        <div>Good night</div>
-      </div>
+      <MainView />
     );
   }
 }
