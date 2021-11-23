0eeefe1e493369484a1e4aba288da0aa94555b29
nightorb <ana.nath@gmx.de>
2021-11-23 16:23:41 +0100

    update files

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -23050,6 +23050,7 @@ class MovieView extends _reactDefault.default.Component {
                     __self: this,
                     children: /*#__PURE__*/ _jsxRuntime.jsx("img", {
                         src: movie.ImagePath,
+                        crossOrigin: "anonymous",
                         __source: {
                             fileName: "src/components/movie-view/movie-view.jsx",
                             lineNumber: 10
