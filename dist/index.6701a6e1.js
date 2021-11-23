689e1ce9738cf01c8c23f3c87fa6b203786d24ee
nightorb <ana.nath@gmx.de>
2021-11-23 16:46:46 +0100

    Revert "update files"
    This reverts commit 0eeefe1e493369484a1e4aba288da0aa94555b29.

--- dist/index.6701a6e1.js
+++ dist/index.6701a6e1.js
@@ -23050,7 +23050,6 @@ class MovieView extends _reactDefault.default.Component {
                     __self: this,
                     children: /*#__PURE__*/ _jsxRuntime.jsx("img", {
                         src: movie.ImagePath,
-                        crossOrigin: "anonymous",
                         __source: {
                             fileName: "src/components/movie-view/movie-view.jsx",
                             lineNumber: 10
