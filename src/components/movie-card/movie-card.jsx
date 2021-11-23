22872319c38cd2951cc72e1f82b196f4f921cd0b
nightorb <ana.nath@gmx.de>
2021-11-23 22:02:55 +0100

    update movie card
    add propTypes

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 export class MovieCard extends React.Component {
   render() {
@@ -7,3 +8,29 @@ export class MovieCard extends React.Component {
     return <div className="movie-card" onClick={() => { onMovieClick(movie); }}>{movie.Title}</div>;
   }
 }
+
+MovieCard.propTypes = {
+  movie: PropTypes.shape({
+    Title: PropTypes.string.isRequired,
+    Description: PropTypes.string.isRequired,
+    ImagePath: PropTypes.string.isRequired
+    // ReleaseYear: PropTypes.string.isRequired,
+    // Genre: PropTypes.shape({
+    //   Name: PropTypes.string.isRequired,
+    //   Description: PropTypes.string.isRequired
+    // }),
+    // Director: PropTypes.shape({
+    //   Name: PropTypes.string.isRequired,
+    //   Bio: PropTypes.string.isRequired,
+    //   BirthYear: PropTypes.string.isRequired,
+    //   Movies: PropTypes.array.isRequired
+    // }),
+    // Actors: PropTypes.shape({
+    //   Name: PropTypes.string.isRequired,
+    //   BirthYear: PropTypes.string.isRequired,
+    //   Movies: PropTypes.array.isRequired
+    // }),
+    // Featured: PropTypes.bool.isRequired
+  }).isRequired,
+  onMovieClick: PropTypes.func.isRequired
+};
