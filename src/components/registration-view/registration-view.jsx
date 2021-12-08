009e5ff6d206ff711b7aa858f4447670cf4391ee
nightorb <89855337+nightorb@users.noreply.github.com>
2021-12-08 22:27:32 +0100

    add logic to registration view
    and added col

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Form, Button, Card } from 'react-bootstrap';
+import axios from 'axios';
+import { Col, Form, Button, Card } from 'react-bootstrap';
 
 export function RegistrationView(props) {
   const [ username, setUsername ] = useState('');
@@ -8,51 +9,65 @@ export function RegistrationView(props) {
   const [ email, setEmail ] = useState('');
   const [ birthday, setBirthday ] = useState('');
 
-  const handleSubmit = e => {
+  const handleRegister = (e) => {
     e.preventDefault();
-    console.log(email, username, password, birthday);
-    props.onRegistration(username);
+    axios.post('https://nightorbs-myflix.herokuapp.com/users', {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthday: birthday
+    })
+    .then(response => {
+      const data = response.data;
+      console.log(data);
+      window.open('/', '_self');
+    })
+    .catch(e => {
+      console.log('error registering the user');
+    });
   }
 
   return (
-    <Card>
-      <Card.Header className="text-center" as="h4">
-        Create a free account
-      </Card.Header>
+    <Col xs={9} md={6}>
+      <Card>
+        <Card.Header className="text-center" as="h4">
+          Create a free account
+        </Card.Header>
 
-      <Card.Body>
-        <Form>
-          <Form.Group className="mb-3" controlId="formUsername">
-            <Form.Label>Username:</Form.Label>
-            <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} required placeholder="Enter a username" />
-          </Form.Group>
+        <Card.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formUsername">
+              <Form.Label>Username:</Form.Label>
+              <Form.Control type="text" value={username} onChange={e => setUsername(e.target.value)} required placeholder="Enter a username" />
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formPassword">
-            <Form.Label>Password:</Form.Label>
-            <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} required placeholder="Your password must be 8 or more characters" />
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formPassword">
+              <Form.Label>Password:</Form.Label>
+              <Form.Control type="password" value={password} onChange={e => setPassword(e.target.value)} required placeholder="Your password must be 8 or more characters" />
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formEmail">
-            <Form.Label>Email:</Form.Label>
-            <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} required placeholder="Enter your email adress" />
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formEmail">
+              <Form.Label>Email:</Form.Label>
+              <Form.Control type="email" value={email} onChange={e => setEmail(e.target.value)} required placeholder="Enter your email adress" />
+            </Form.Group>
 
-          <Form.Group className="mb-3" controlId="formBirthday">
-            <Form.Label>Birthday:</Form.Label>
-            <Form.Control type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
-          </Form.Group>
+            <Form.Group className="mb-3" controlId="formBirthday">
+              <Form.Label>Birthday:</Form.Label>
+              <Form.Control type="date" value={birthday} onChange={e => setBirthday(e.target.value)} />
+            </Form.Group>
 
-          <Button variant="primary" type="submit" onClick={handleSubmit}>Sign up</Button>
-        </Form>
-      </Card.Body>
+            <Button variant="primary" type="submit" onClick={handleRegister}>Sign up</Button>
+          </Form>
+        </Card.Body>
 
-      <Card.Footer className="text-muted text-center">
-        <Card.Text>
-          Already have an account?
-        </Card.Text>
-        <Button variant="secondary">Login</Button>
-      </Card.Footer>
-    </Card>
+        <Card.Footer className="text-muted text-center">
+          <Card.Text>
+            Already have an account?
+          </Card.Text>
+          <Button variant="secondary">Login</Button>
+        </Card.Footer>
+      </Card>
+    </Col>
   );
 }
 
