232dbfc6a0d361ea7645cad62f94c21090730176
nightorb <ana.nath@gmx.de>
2021-12-20 21:43:04 +0100

    update profile-view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,59 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
 import { Row, Col, Form, Button, Card } from 'react-bootstrap';
 
-export function ProfileView({ movies, onUpdatedUserInfo }) {
-  const [ user, setUser ] = useState('');
+export function ProfileView({ user, setUser, movies, onUpdatedUserInfo, onLoggedOut }) {
+  // const [ user, setUser ] = useState('');
 
   const [ username, setUsername ] = useState('');
   const [ password, setPassword ] = useState('');
   const [ email, setEmail ] = useState('');
   const [ birthday, setBirthday ] = useState('');
+  const [ favoriteMovies, setFavoriteMovies ] = useState('');
 
-  // const favoriteMovies = movies.filter(() => {
+  const token = localStorage.getItem('token');
 
-  // });
-  const removeFavorite = (id) => {
-    let token = localStorage.getItem('token');
-    let url = `https://nightorbs-myflix.herokuapp.com/users/${localStorage.getItem('user')}/movies/${_id}`;
-    axios.delete(url, {
-      headers: { Authorization: `Bearer ${token}` }
-    })
-  }
-
-  const getUser = () => {
-
+  const updateUser = (e) => {
+    e.preventDefault();
+    axios.put('https://nightorbs-myflix.herokuapp.com/users/${user.Username}',
+      {
+        Username: username,
+        Password: password,
+        Email: email,
+        Birthday: birthday
+      }, {
+        headers: { Authorization: `Bearer ${token}` }
+      })
+      .then(response => {
+        console.log(response.data);
+        setUser(response.data);
+        window.open('/', '_self');
+      })
+      .catch(err => {
+        console.log('error updating user information');
+      });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefaut();
-    axios.put('https://nightorbs-myflix.herokuapp.com/users/${user.Username}', {
-      Username: username,
-      Password: password,
-      Email: email,
-      Birthday: birthday
-    }, {
+  const deleteUser = (e) => {
+    axios.delete('https://nightorbs-myflix.herokuapp.com/users/${user.Username}', {
       headers: { Authorization: `Bearer ${token}` }
     })
     .then(response => {
-      const data = response.data;
-      console.log(data);
-      setUser(data);
-      window.open('/', '_self');
+      console.log(response.data);
+      onLoggedOut();
     })
-    .catch(e => {
-      console.log('error updating user information');
-    });
-  };
+    .catch(err => {
+      console.error(err);
+    })
+  }
 
-  const handleUpdate = (e) => {
-
-  };
-
-  useEffect(() => {
-
-  }, [])
+  const removeFavorite = (id) => {
+    axios.delete(`https://nightorbs-myflix.herokuapp.com/users/${user.Username}/movies/${_id}`, {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+  }
 
   return (
     <Row>
@@ -61,23 +60,66 @@ export function ProfileView({ movies, onUpdatedUserInfo }) {
         <Card>
           <Card.Body>
             <h4>Your Info</h4>
-            <p>Username: {user.Username}</p>
-            <p>Email: {user.Email}</p>
+            <div className="user-username">
+              <span className="label">Username: </span>
+              <span className="value">{user.Username}</span>
+            </div>
+            <div className="user-email">
+              <span className="label">Email: </span>
+              <span className="value">{user.Email}</span>
+            </div>
           </Card.Body>
         </Card>
       </Col>
       <Col xs={12} sm={8}>
         <Card>
+          <Card.Header className="text-center" as="h4">
+            Edit Profile
+          </Card.Header>
+
           <Card.Body>
-            <Form className="profile-form" onSubmit={(e) => handleSubmit(e)}>
-            <h4>Update your info</h4>
-            <label>Username:</label>
-            <input type="text" name="Username" defaultValue={user.Username} onChange={e => handleUpdate(e)} />
-            <label>Password:</label>
-            <input type="password" name="Password" defaultValue={user.Password} onChange={e => handleUpdate(e)} />
-            <label>Email:</label>
-            <input type="email" name="Email" defaultValue={user.Email} onChange={e => handleUpdate(e)} />
-          </Form>
+            <Form className="profile-form">
+              <Form.Group className="mb-3" controlId="formUsername">
+                <Form.Label>Username:</Form.Label>
+                <Form.Control
+                  type="text"
+                  value={username}
+                  onChange={e => setUsername(e.target.value)}
+                  placeholder="Enter a new username"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formPassword">
+                <Form.Label>Password:</Form.Label>
+                <Form.Control
+                  type="password"
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                  placeholder="Your password must be 8 or more characters"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formEmail">
+                <Form.Label>Email:</Form.Label>
+                <Form.Control
+                  type="email"
+                  value={email}
+                  onChange={e => setEmail(e.target.value)}
+                  placeholder="Enter a new email adress"
+                />
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="formBirthday">
+                <Form.Label>Birthday:</Form.Label>
+                <Form.Control
+                  type="date"
+                  value={birthday}
+                  onChange={e => setBirthday(e.target.value)}
+                />
+              </Form.Group>
+
+              <Button variant="primary" type="submit" onClick={updateUser}>Update</Button>
+            </Form>
           </Card.Body>
         </Card>
       </Col>
