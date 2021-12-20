00377c5d03f1bca301cf03484617c0899dbc6d93
nightorb <ana.nath@gmx.de>
2021-12-20 21:42:26 +0100

    update movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -27,23 +27,6 @@ MovieCard.propTypes = {
     Title: PropTypes.string.isRequired,
     Description: PropTypes.string.isRequired,
     ImagePath: PropTypes.string.isRequired,
-    ReleaseYear: PropTypes.string.isRequired,
-    Genre: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Description: PropTypes.string.isRequired
-    }),
-    Director: PropTypes.shape({
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired,
-      BirthYear: PropTypes.string.isRequired,
-      Movies: PropTypes.array.isRequired
-    }),
-    Actors: PropTypes.arrayOf(
-      PropTypes.shape({
-        Name: PropTypes.string.isRequired,
-        BirthYear: PropTypes.string.isRequired,
-        Movies: PropTypes.array.isRequired
-      })),
-    Featured: PropTypes.bool.isRequired
+    ReleaseYear: PropTypes.string.isRequired
   }).isRequired
 };
