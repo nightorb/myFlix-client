d299cab1e75d0f25da83db9993606e7149109180
nightorb <ana.nath@gmx.de>
2022-01-07 15:57:07 +0100

    update movie-view
    add addFavoriteMovie function, fix actors array

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,45 +1,74 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import { Link } from 'react-router-dom';
-import { Col, Button, Image } from 'react-bootstrap';
+import { Button, Image } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  addFavoriteMovie() {
+    const token = localStorage.getItem('token'),
+      username = localStorage.getItem('user');
+
+    axios.post(`https://nightorbs-myflix.herokuapp.com/users/${username}/favorites/${this.props.movie._id}`, {}, {
+      headers: { Authorization: `Bearer ${token}` },
+      method: 'POST'
+    })
+    .then(response =>{
+      alert('Movie added to your favorites');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+  };
+
   render() {
     const { movie, onBackClick } = this.props;
 
     return (
-      <Col md={8} lg={4}>
-        <div className="movie-view">
-          <div className="movie-poster">
-            <Image src={movie.ImagePath} crossOrigin="anonymous" fluid />
-          </div>
-          <div className="movie-title">
-            <span className="value">{movie.Title}</span>
-          </div>
-          <div className="movie-description">
-            <span className="value">{movie.Description}</span>
-          </div>
-          <div className="movie-genre">
-            <span className="label">Genre: </span>
-            <Link to={`/genres/${movie.Genre.Name}`}>
-              <Button variant="link">{movie.Genre.Name}</Button>
-            </Link>
-          </div>
-          <div className="movie-director">
-            <span className="label">Director: </span>
-            <Link to={`/directors/${movie.Director.Name}`}>
-              <Button variant="link">{movie.Director.Name}</Button>
-            </Link>
-          </div>
-          <div className="movie-actors">
-            <span className="label">Actors: </span>
-            <Link to={`/actors/${movie.Actors.Name}`}>
-              <Button variant="link">{movie.Actors.Name}</Button>
-            </Link>
-          </div>
-          <Button variant="primary" onClick={() => { onBackClick(); }}>Back</Button>
+      <div className="movie-view">
+        <div className="movie-poster">
+          <Image src={movie.ImagePath} crossOrigin="anonymous" fluid />
         </div>
-      </Col>
+
+        <div className="movie-title">
+          <span className="value">{movie.Title}</span>
+        </div>
+
+        <div className="movie-description">
+          <span className="value">{movie.Description}</span>
+        </div>
+
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <Link to={`/genres/${movie.Genre.Name}`}>
+            <Button variant="link">{movie.Genre.Name}</Button>
+          </Link>
+        </div>
+
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <Link to={`/directors/${movie.Director.Name}`}>
+            <Button variant="link">{movie.Director.Name}</Button>
+          </Link>
+        </div>
+
+        <div className="movie-actors">
+          <span className="label">Actors: </span>
+          { movie.Actors.map((actor) => (
+            <Link key={actor.Name} to={`/actors/${actor.Name}`}>{actor.Name}</Link> ))
+            .reduce((prev, curr) => [ prev, ", ", curr ])
+          }
+        </div>
+
+        <Button variant="danger" value={movie._id} onClick={(e) => this.addFavoriteMovie(e, movie)}>Add to Favorites</Button>
+
+        <Button variant="primary" onClick={() => { onBackClick(); }}>Back</Button>
+      </div>
     );
   }
 }
