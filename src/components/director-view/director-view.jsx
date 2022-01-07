d36d42e361aaac0ea48a1838faf2021a1199b4c5
nightorb <ana.nath@gmx.de>
2022-01-07 15:54:27 +0100

    update director view

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 import { Col, Button, Image } from 'react-bootstrap';
 
 export class DirectorView extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     const { director, onBackClick } = this.props;
 
@@ -13,15 +17,22 @@ export class DirectorView extends React.Component {
           <div className="director-name">
             <span className="value">{director.Name}</span>
           </div>
+
           <div className="director-bio">
             <span className="value">{director.Bio}</span>
           </div>
+
           <div className="director-birthyear">
             <span className="value">{director.BirthYear}</span>
           </div>
+
           <div className="director-movies">
             <span className="label">Movies: </span>
-          </div>
+            { director.Movies.map((movie) => (
+              <Link key={movie._id} to={`/movies/${movie._id}`}>{movie.Title}</Link> ))
+              .reduce((prev, curr) => [ prev, ", ", curr ])
+            }
+         </div>
 
           <Button variant="primary" onClick={() => { onBackClick(); }}>Back</Button>
         </div>
@@ -34,6 +45,12 @@ DirectorView.propTypes = {
   director: PropTypes.shape({
     Name: PropTypes.string.isRequired,
     Bio: PropTypes.string.isRequired,
-    BirthYear: PropTypes.string.isRequired
+    BirthYear: PropTypes.string.isRequired,
+    Movies: PropTypes.arrayOf(
+      PropTypes.shape({
+        Title: PropTypes.string.isRequired,
+        ImagePath: PropTypes.string.isRequired,
+        ReleaseYear: PropTypes.string.isRequired
+      }))
   }).isRequired
 };
