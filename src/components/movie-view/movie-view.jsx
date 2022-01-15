6bd1adacbfd63fd179743d899e81e25bda8a51a2
nightorb <ana.nath@gmx.de>
2022-01-15 21:02:24 +0100

    update movie-view

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom';
 import { Button, Image } from 'react-bootstrap';
 
 export class MovieView extends React.Component {
-
   constructor(props) {
     super(props);
   }
