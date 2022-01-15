dd60af8b80aee942537826739487244a22044188
nightorb <ana.nath@gmx.de>
2022-01-15 20:07:33 +0100

    update navbar-view

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -4,19 +4,19 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 export function NavbarView() {
   const user = localStorage.getItem('user');
 
-  onLoggedOut = () => {
+  const onLoggedOut = () => {
     localStorage.clear();
     window.open('/', '_self');
   }
 
   return (
-  <Navbar className="mb-5" bg="primary" variant="dark">
-    <Navbar.Brand href="/">myFlix</Navbar.Brand>
-    <Nav className="me-auto">
-      <Nav.Link href="/">Movies</Nav.Link>
-      <Nav.Link href={`/users/${user}`}>Profile</Nav.Link>
-      <Button onClick={() => { this.onLoggedOut() }}>Logout</Button>
-    </Nav>
-  </Navbar>
+    <Navbar className="mb-5" bg="primary" variant="dark">
+      <Navbar.Brand href="/">myFlix</Navbar.Brand>
+      <Nav className="me-auto">
+        <Nav.Link href="/">Movies</Nav.Link>
+        <Nav.Link href={`/users/${user}`}>Profile</Nav.Link>
+        <Button onClick={() => { onLoggedOut() }}>Logout</Button>
+      </Nav>
+    </Navbar>
   );
 }
