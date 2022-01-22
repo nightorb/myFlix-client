a9779dd115b61afd9fc2e3e5e8bff0482ca8f851
nightorb <ana.nath@gmx.de>
2022-01-22 01:49:39 +0100

    styling navbar

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
 
 import './navbar-view.scss';
 
@@ -12,13 +12,26 @@ export function NavbarView() {
   }
 
   return (
-    <Navbar variant="dark">
-      <Navbar.Brand href="/">myFlix</Navbar.Brand>
-      <Nav className="me-auto">
-        <Nav.Link href="/">Movies</Nav.Link>
-        <Nav.Link href={`/users/${user}`}>Profile</Nav.Link>
-        <Button className="shadow-none" onClick={() => { onLoggedOut() }}>Logout</Button>
-      </Nav>
+    <Navbar expand="md">
+      <Container>
+        <Navbar.Brand href="/">myFlix</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            <NavDropdown className="navbar-dropdown" title="Movies" href="/">
+              <NavDropdown.Item href="/">Movies</NavDropdown.Item>
+              <NavDropdown.Item href="/genres">Genres</NavDropdown.Item>
+              <NavDropdown.Item href="/directors">Directors</NavDropdown.Item>
+              <NavDropdown.Item href="/actors">Actors</NavDropdown.Item>
+            </NavDropdown>
+            <NavDropdown className="navbar-dropdown" title="Account" href="/">
+              <NavDropdown.Item href={`/users/${user}`}>Profile</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item onClick={() => { onLoggedOut() }}>Logout</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   );
 }
