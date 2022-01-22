3c01bb26cae78a2d4a69900610ba6573f01da3fb
nightorb <ana.nath@gmx.de>
2022-01-22 20:51:20 +0100

    styling movie-card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -13,7 +13,7 @@ export class MovieCard extends React.Component {
       <Card className="movie-card mb-4">
         <Card.Img variant="top" src={movie.ImagePath} />
         <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
+          <Card.Title className="text-truncate">{movie.Title}</Card.Title>
           <Card.Text className="muted-text">{movie.ReleaseYear}</Card.Text>
           <Link to={`/movies/${movie._id}`}>
             <Button className="button-primary shadow-none">More</Button>
