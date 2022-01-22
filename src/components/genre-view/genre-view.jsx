9e871b27d3a401afabe9995bfb199559f4ea82f1
nightorb <ana.nath@gmx.de>
2022-01-22 01:47:08 +0100

    styling genre-view

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -2,22 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Col, Button } from 'react-bootstrap';
 
+import './genre-view.scss';
+
 export class GenreView extends React.Component {
 
   render() {
     const { genre, onBackClick } = this.props;
 
     return (
-      <Col md={8} lg={4}>
+      <Col md={8}>
         <div className="genre-view">
-          <div className="genre-name">
-            <span className="value">{genre.Name}</span>
-          </div>
-          <div className="genre-description">
-            <span className="value">{genre.Description}</span>
-          </div>
-  
-          <Button variant="primary" onClick={() => { onBackClick(); }}>Back</Button>
+          <div className="genre-name mb-4">{genre.Name}</div>
+          <div className="genre-description mb-4 mb-md-5">{genre.Description}</div>
+          <Button className="button-primary" onClick={() => { onBackClick(); }}>Back</Button>
         </div>
       </Col>
     );
