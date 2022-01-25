b922346b658effcad0849125113925fe93e5ad24
nightorb <ana.nath@gmx.de>
2022-01-26 00:19:00 +0100

    update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux'
+import movieApp from './reducers/reducers';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import { Container } from 'react-bootstrap';
 
 import MainView from './components/main-view/main-view';
@@ -7,13 +11,17 @@ import MainView from './components/main-view/main-view';
 // import statement to indicate that index.scss needs to be bundled
 import './index.scss';
 
+const store = createStore(movieApp, devToolsEnhancer());
+
 // main component which uses all the others
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container className="px-0" fluid>
-        <MainView />
-      </Container>
+      <Provider store={store}>
+        <Container className="px-0" fluid>
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
