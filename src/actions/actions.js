9b4c929a86d390801bd24b9bc0afe75de4016a12
nightorb <ana.nath@gmx.de>
2022-01-26 00:18:41 +0100

    update actions

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,10 +1,35 @@
 export const SET_MOVIES = 'SET_MOVIES';
+export const SET_GENRES = 'SET_GENRES';
+export const SET_DIRECTORS = 'SET_DIRECTORS';
+export const SET_ACTORS = 'SET_ACTORS';
 export const SET_FILTER = 'SET_FILTER';
+export const SET_USER = 'SET_USER';
+export const UPDATE_USER = 'UPDATE_USER';
 
 export function setMovies(value) {
-  console.log('SET_MOVIES action triggered');
   return {
     type: SET_MOVIES,
+    value
+  }
+}
+
+export function setGenres(value) {
+  return {
+    type: SET_GENRES,
+    value
+  }
+}
+
+export function setDirectors(value) {
+  return {
+    type: SET_DIRECTORS,
+    value
+  }
+}
+
+export function setActors(value) {
+  return {
+    type: SET_ACTORS,
     value
   }
 }
@@ -12,6 +37,20 @@ export function setMovies(value) {
 export function setFilter(value) {
   return {
     type: SET_FILTER,
+    value
+  }
+}
+
+export function setUser(value) {
+  return {
+    type: SET_USER,
+    value
+  }
+}
+
+export function updateUser(value) {
+  return {
+    type: UPDATE_USER,
     value
   }
 }
