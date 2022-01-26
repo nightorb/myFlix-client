4d208b00ae4631cbad589c974f998cb770ada108
nightorb <ana.nath@gmx.de>
2022-01-26 22:23:20 +0100

    update navbar
    include visibility filter input

--- src/components/navbar-view/navbar-view.jsx
+++ src/components/navbar-view/navbar-view.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap';
+
+import VisibilityFilterInput from '../visibility-filter-input/visibility-filter-input';
 
 import './navbar-view.scss';
 
-export function NavbarView() {
+export function NavbarView({visibilityFilter}) {
   const user = localStorage.getItem('user');
 
   const onLoggedOut = () => {
@@ -16,22 +19,34 @@ export function NavbarView() {
       <Container>
         <Navbar.Brand href="/">myFlix</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse>
-          <Nav className="me-auto">
+
+        <Navbar.Collapse >
+          <Nav className="align-items-md-center me-auto">
             <NavDropdown className="navbar-dropdown" title="Movies" href="/">
               <NavDropdown.Item href="/">Movies</NavDropdown.Item>
               <NavDropdown.Item href="/genres">Genres</NavDropdown.Item>
               <NavDropdown.Item href="/directors">Directors</NavDropdown.Item>
               <NavDropdown.Item href="/actors">Actors</NavDropdown.Item>
             </NavDropdown>
-            <NavDropdown className="navbar-dropdown" title="Account" href="/">
+            <NavDropdown className="navbar-dropdown mb-3 mb-md-0" title="Account" href="/">
               <NavDropdown.Item href={`/users/${user}`}>Profile</NavDropdown.Item>
               <NavDropdown.Divider />
               <NavDropdown.Item onClick={() => { onLoggedOut() }}>Logout</NavDropdown.Item>
             </NavDropdown>
+            <Nav.Item className="justify-content-end d-flex d-md-none">
+              <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+            </Nav.Item>
           </Nav>
         </Navbar.Collapse>
+
+        <Nav.Item className="nav-item-large justify-content-end d-none d-md-flex">
+          <VisibilityFilterInput visibilityFilter={visibilityFilter} />
+        </Nav.Item>
       </Container>
     </Navbar>
   );
+}
+
+NavbarView.propTypes = {
+  visibilityFilter: PropTypes.func 
 }
