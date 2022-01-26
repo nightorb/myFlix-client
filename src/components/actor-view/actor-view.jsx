d21161bacb5e865404022fde74587e62da5eb1e4
nightorb <ana.nath@gmx.de>
2022-01-26 22:22:05 +0100

    styling actor-vie

--- src/components/actor-view/actor-view.jsx
+++ src/components/actor-view/actor-view.jsx
@@ -14,28 +14,28 @@ export class ActorView extends React.Component {
     return (
       <div className="actor-view">
         <Row className="justify-content-center">
-          <Col md={10} lg={8}>
+          <Col md={10} lg={9}>
             <div className="actor-name mb-4">{actor.Name}</div>
             <div className="actor-birthyear mb-4">Birthyear: {actor.BirthYear}</div>
           </Col>
         </Row>
 
         <Row className="justify-content-center">
-          <Col className="mb-3 mb-md-4" md={10} lg={8}>
+          <Col className="mb-3 mb-md-4" md={10} lg={9}>
             <div className="actor-movies">Movies:</div>
           </Col>
 
           <div className="w-100" />
 
           { actor.Movies.map(movie => (
-            <Col className="movie-card-container d-flex align-items-stretch mb-4 mb-md-5" sm={6} md={5} lg={4} key={movie._id}>
+            <Col className="movie-card-container d-flex align-items-stretch mb-4 mb-md-5" sm={6} md={5} lg={3} key={movie._id}>
               <MovieCard movie={movie.Movie} role={movie.Role} />
             </Col>
           ))}
 
           <div className="w-100" />
 
-          <Col md={10} lg={8}>
+          <Col md={10} lg={9}>
             <Button className="button-primary" onClick={() => { onBackClick(); }}>Back</Button>
           </Col>
         </Row>
