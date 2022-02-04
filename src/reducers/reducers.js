6b55960b969187573ac5fdfcf2cf26fd34077503
nightorb <ana.nath@gmx.de>
2022-02-04 23:53:57 +0100

    update reducers

--- src/reducers/reducers.js
+++ src/reducers/reducers.js
@@ -1,11 +1,19 @@
 import { combineReducers } from 'redux';
-import { SET_MOVIES, SET_GENRES, SET_DIRECTORS, SET_ACTORS, SET_FILTER, SET_USER, UPDATE_USER } from '../actions/actions';
+import { 
+  SET_MOVIES,
+  SET_GENRES,
+  SET_DIRECTORS,
+  SET_ACTORS,
+  SET_FILTER,
+  SET_USER,
+  UPDATE_USER,
+  ADD_FAVORITE
+} from '../actions/actions';
 
 function movies(state = [], action) {
   switch(action.type) {
     case SET_MOVIES:
       return action.value;
-
     default:
       return state;
   }
@@ -15,7 +23,6 @@ function genres(state = [], action) {
   switch(action.type) {
     case SET_GENRES:
       return action.value;
-
     default:
       return state;
   }
@@ -25,7 +32,6 @@ function directors(state = [], action) {
   switch(action.type) {
     case SET_DIRECTORS:
       return action.value;
-
     default:
       return state;
   }
@@ -35,7 +41,6 @@ function actors(state = [], action) {
   switch(action.type) {
     case SET_ACTORS:
       return action.value;
-
     default:
       return state;
   }
@@ -45,20 +50,27 @@ function visibilityFilter(state = '', action) {
   switch(action.type) {
     case SET_FILTER:
       return action.value;
-
     default:
       return state;
   }
 }
 
-function user(state = [], action) {
+function user(state = null, action) {
   switch(action.type) {
     case SET_USER:
       return action.value;
-
     case UPDATE_USER:
-      return action.value;
-      
+      // takes state and action.value and combines them into a new object
+      return {...state, ...action.value};
+    default:
+      return state;
+  }
+}
+
+function addFavorite(state = [], action) {
+  switch(action.type) {
+    case ADD_FAVORITE:
+      return {...state, ...action.value};
     default:
       return state;
   }
@@ -70,7 +82,8 @@ const movieApp = combineReducers({
   directors,
   actors,
   visibilityFilter,
-  user
+  user,
+  addFavorite
 });
 
 export default movieApp;
